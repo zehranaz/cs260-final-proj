98739874b18f6fa2fa59cbe8662b7e983c3491a9
zehranaz@college.harvard.edu <zehranaz@college.harvard.edu>
2015-04-07 03:02:42 -0400

    added klee_assume statment to main

--- klee_string_test.c
+++ klee_string_test.c
@@ -9,8 +9,30 @@
 
 #define SIZE 3
 
+
+char* string_concat(char* front, char* back) {
+
+	if (!front || !back) {
+		printf("error! please provide valid inputs. \n");
+		return NULL;
+	}
+
+	int front_count = strlen(front);
+	int back_count = strlen(back);
+
+	char* return_val;
+	return_val = malloc(front_count + back_count + 1);
+	if (!return_val)
+		return NULL;
+
+	char* combined = strcat(front, back);
+	strcpy(return_val, combined);
+	return return_val;
+
+}
+
 int find_needle_in_haystack(char *hay1, char* hay2, char* needle) {
-	char* haystack = strcat(hay1, hay2);
+	char* haystack = string_concat(hay1, hay2);
 	if (strcmp(needle, haystack) == 0) {
 		return 1;
 	}
@@ -26,25 +48,10 @@ int main() {
 	char needle[] = "car";
 	klee_make_symbolic(hay1, SIZE, "hay1");
 	klee_make_symbolic(hay2, SIZE, "hay2");
+
+	// assume null-terminated strings
+	klee_assume(hay1[SIZE-1] == '\0');
+	klee_assume(hay2[SIZE-1] == '\0');
 	//klee_make_symbolic(needle, SIZE, "needle");
 	return find_needle_in_haystack(hay1, hay2, needle);
 }
-
-
-// char* string_concat(char* front, char* back) {
-
-// 	if (!front || !back) {
-// 		printf("error! please provide valid inputs. \n");
-// 		return NULL;
-// 	}
-
-// 	int front_count = strlen(front);
-// 	int back_count = strlen(back);
-
-// 	char* return_val;
-// 	return_val = malloc(front_count + back_count + 1);
-// 	char* combined = strcat(front, back);
-// 	strcpy(return_val, combined);
-// 	return return_val;
-
-// }
