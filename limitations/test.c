176228470411bb3f9fcf2aa6f4ef68c364a1add0
Anitha Gollamudi <anitha.gollamudi@gmail.com>
2015-04-08 09:25:43 -0400

    Added testcases that demonstrate klee's limitations

---
+++ limitations/test.c
@@ -0,0 +1,17 @@
+#include <klee/klee.h>
+
+int get_sign(char x) {
+  if (x == 0)
+     return 0;
+  
+  if (x < 0)
+     return -1;
+  else 
+     return 1;
+} 
+
+int main() {
+  char a;
+  klee_make_symbolic(&a, sizeof(a), "a");
+  return get_sign(a);
+} 
